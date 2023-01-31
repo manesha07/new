ec2a9d51f826b0d5510996765a256e591f2080d3
manesha07 <manishagora52@gmail.com>
2023-01-31 14:02:19 +0545

    new

---
+++ frontend/src/Component/Navbar.js
@@ -0,0 +1,72 @@
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
+import { useState, useEffect } from "react";
+import { authService } from "../authentication/authentication";
+import img from "../image/e.png"
+
+export const Navbar = () => {
+    const cartCount =useSelector(state => {
+        console.log("this is nav state",state.cart.count) 
+        return state.cart.count})
+        const [currentUser, setCurrentUser] = useState(undefined);
+  console.log("currenttt",currentUser);
+
+  useEffect(() => {
+    const user = authService.getCurrentUser();
+    console.log("userla",user)
+
+    if (user) {
+      setCurrentUser(user.currentUser);
+    }
+  }, []);
+
+  const logOut = () => {
+    authService.logout();
+    window.location.reload();
+  };  
+    return <>
+    <div className="nav flex flex-wrap justify-between bg-[#e8e8e8]">
+        <div className="flex justify-start inline-block">
+        <img src={img} alt="hero img" className='h-[70px] w-[70px] pt-[5px]'/>
+        </div>
+        <ul className="inline-block flex text-grey justify-end py-[20px] items-center">
+            <li className="p-[10px] hover:bg-[orange] "> <Link to = "/">Home</Link></li>
+            <li className="p-[10px] hover:bg-[orange] "> <Link to ="/">Contact</Link></li>
+            <li className="p-[10px] hover:bg-[orange] "> <Link to ="/products"> Our Products </Link></li>
+                                 {currentUser ? (
+                                    currentUser === 'user'?
+                                    <>
+                                <Link to="/cart" >
+                                      <li className="p-[10px] hover:bg-[orange] "> <Link to ="/cart"> Cart({cartCount}) </Link></li>
+                                    </Link>                           
+                                    <Link to="/login">
+                                         <li  className="p-[10px] hover:bg-[orange] " onClick ={logOut}>  Logout </li>
+                                        </Link>
+                                      </>:
+                                      <>
+                                                <Link to="/dashboard" >
+                                               <li className="p-[10px] hover:bg-[orange] "> Dashboard </li>
+                                               </Link>                           
+                                               <Link to="/login">
+                                                   <li onClick ={logOut} className="p-[10px] hover:bg-[orange] ">  Logout </li>
+                                                   </Link>
+                                                   </> 
+                                   ): 
+                                      <>
+                                       <Link to="/register" className="">
+                                               <li className="p-[10px] hover:bg-[orange] "> Sign Up</li> 
+                                       </Link>
+                                       <div className="dropdown"><li className="last small-content dropbtn">
+                                               Login 
+                                              <div className="dropdown-content">
+                                                  <Link to="/login">Admin </Link>
+                                                   <Link to="/userLogin">Customer </Link>
+                                                   </div>
+               
+                                               </li>
+                                              </div>
+                                       </>}
+        </ul>
+    </div>
+    </>
+}
